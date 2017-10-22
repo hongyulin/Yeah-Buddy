d263478526b83f703af05b6b7607079dc56defa8
Levinson <389169974@qq.com>
2017-10-22 20:09:58 +0800

    基本框架

--- yeahbuddy/src/views/show/showBoutique.vue
+++ yeahbuddy/src/views/show/showBoutique.vue
@@ -1,6 +1,101 @@
 <template>
 	<div>
-
+		<section>
+			<!-- 走马灯 -->
+		</section>
+		<section>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>秀身材</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>秀记录</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>人气榜</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>挑战活动</figcaption>
+			</figure>
+		</section>
+		<section>
+			<title>
+				<span>经验分享</span>
+				<img src="static/img/.svg" alt="">
+			</title>
+			<ul>
+				<li v-for="">
+					<seciton>
+						<img src="static/img/.svg" alt="">
+						<div>
+							<p></p>
+							<footer>
+								<img src="static/img/.svg" alt="">
+								<span>
+									<img src="static/img/.svg" alt="">
+									{{}}
+								</span>
+								<span>
+									<img src="static/img/.svg" alt="">
+									{{}}
+								</span>
+							</footer>
+						</div>
+					</seciton>
+				</li>
+			</ul>
+		</section>
+		<section>
+			<title>
+				<span>热门话题</span>
+				<img src="static/img/.svg" alt="">
+			</title>
+			<ul>
+				<li v-for="">
+					<seciton>
+						<img src="static/img/.svg" alt="">
+						<div>
+							<title></title>
+							<p></p>
+							<footer>
+								<img src="static/img/.svg" alt="">
+								<img src="static/img/.svg" alt="">
+								<img src="static/img/.svg" alt="">
+								<img src="static/img/.svg" alt="">
+								{{}}正在参与
+							</footer>
+						</div>
+					</seciton>
+				</li>
+			</ul>
+		</section>
+		<section>
+			<title>小八精选</title>
+			<ul>
+				<!-- 中间加一些推荐 -->
+				<li v-for="">
+					<section>
+						<figure>
+							<img src="static/img/.svg" alt="">
+							<figcaption></figcaption>
+						</figure>
+						<footer>
+							<span>
+								<img src="static/img/.svg" alt="" class="">
+								{{}}
+							</span>
+							<span>
+								<img src="static/img/.svg" alt="" class="">
+								{{}}
+							</span>
+						</footer>
+					</section>
+				</li>
+			</ul>
+		</section>
 	</div>
 </template>
 <script>
--- yeahbuddy/src/views/show/showTrends.vue
+++ yeahbuddy/src/views/show/showTrends.vue
@@ -1,6 +1,68 @@
 <template>
 	<div>
+		<seciton>
+			<title>
+				他们刚刚在练
+				<img src="static/img/.svg" alt="">
+			</title>
+			<!-- 这儿放一个横向的无滚条的list -->
+		</seciton>
+		<seciton>
+			<ul>
+				<li>
+					<router-link to="#">
+						<title>
+							<img src="static/img/.svg" alt="" class="">
+							由@小八推荐
+						</title>
+						<hr>
+						<section>
+							<img src="static/img/.svg" alt="">
+							<span>
+								<!-- 放名字和时间 -->
+								{{}}
+								<br>
+								{{}}
+							</span>
+							<button>+关注</button>
+						</section>
+						<figure>
+							<img src="static/img/.svg" alt="">
+							<figcaption></figcaption>
+							<!-- 文章时候显示 -->
+							<span v-show="">{{}}次阅览</span>
+							<p></p>
+							<!-- 参加的是计划的时候显示 -->
+							<div>
+								<title>
+									<!-- 放表情 -->
+									<img src="static/img/.svg" alt="">
+									<span>计划第{{}}天，完成</span>
+									<!-- 这儿放计划的名字 -->
+								</title>
+								<hr>
+								<div>
+									<span>{{}}<br>动作/组</span>
+									<span>{{}}<br>本次训练/分钟</span>
+									<span>{{}}<br>本次燃脂/千卡</span>
+								</div>
+								<span>打卡第{{}}天</span>
+							</div>
+						</figure>
+						<footer>
+							<span><img src="static/img/.svg" alt="">{{}}</span>
+							<span><img src="static/img/.svg" alt="">{{}}</span>
+							<span><img src="static/img/.svg" alt="">{{}}</span>
+						</footer>
+						
 
+					</router-link>
+				</li>
+			</ul>
+		</seciton>
+		<seciton>
+
+		</seciton>
 	</div>
 </template>
 <script>
