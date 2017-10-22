b51aa0389060fd9abc21667a54f9de68fbec3288
Levinson <389169974@qq.com>
2017-10-22 15:36:00 +0800

    refresh

--- yeahbuddy/src/views/circle/circleC.vue
+++ yeahbuddy/src/views/circle/circleC.vue
@@ -4,16 +4,103 @@
 			<!-- 走马灯 -->
 		</section>
 		<section>
-			
+			<title>
+				<span>我的圈子</span>
+				<span>
+					查看全部
+					<img src="static/img//svg" alt="">
+				</span>
+			</title>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>上海悦动圈</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>跑步装备圈</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>跑步圈</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>加入圈子</figcaption>
+			</figure>
 		</section>
 		<section>
-
+			<title>
+				<span>圈子活动</span>
+				<span>
+					查看全部
+					<img src="static/img/.svg" alt="">
+				</span>
+			</title>
 		</section>
 		<section>
-
+			<title>精选内容</title>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>运动百科</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>话题讨论</figcaption>
+			</figure>
+			<figure>
+				<img src="static/img/.svg" alt="">
+				<figcaption>悦友故事</figcaption>
+			</figure>
 		</section>
 		<section>
-
+			<title>
+				<span>热门话题</span>
+				<span>
+					查看全部
+					<img src="static/img/.svg" alt="">
+				</span>
+			</title>
+			<ul>
+				<li v-for="">
+					<!-- 中间夹用户推荐，，， -->
+					<header>
+						<img src="static/img/.svg" alt="">
+						<div>
+							<span>
+								{{}}
+								<img src="static/img/.svg" alt="">
+							</span>
+							<span class="">
+								{{}}
+							</span>
+						</div>
+						<button>关注</button>
+					</header>
+					<div>
+						<p></p>
+						<div>
+							<img src="static/img/.svg" alt="">
+							<img src="static/img/.svg" alt="">
+							<img src="static/img/.svg" alt="">
+						</div>
+						<span class="pisition">{{}}</span>
+					</div>
+					<footer>
+						<span>
+							<img src="" alt="">
+							{{}}
+						</span>
+						<span>
+							<img src="" alt="">
+							{{}}
+						</span>
+						<span>
+							<img src="" alt="">
+							{{}}
+						</span>
+					</footer>
+				</li>
+			</ul>
 		</section>
 	</div>
 </template>
--- yeahbuddy/src/views/circle/circleTrends.vue
+++ yeahbuddy/src/views/circle/circleTrends.vue
@@ -1,6 +1,54 @@
 <template>
+	<!-- 这里面放我发布的动态，关注的人的动态，和加入的圈子的动态 -->
 	<div>
-
+		<section>
+			<title>
+				<span>推荐关注:</span>
+				<img src="static/img/.svg" alt="">
+			</title>
+			<hr>
+			<div>
+				<img src="static/img/.svg" alt="">
+				<div>
+					<span>{{}}[热门印花赛圈主]</span>
+					<span>成功组织{{}}次印花赛</span>
+				</div>
+				<button>{{}}</button>
+			</div>
+		</section>
+		<section>
+			<ul>
+				<li v-for="">
+					<seciton>
+						<router-link>
+							<img src="static/img/.svg" alt="">
+						</router-link>
+					</seciton>
+					<seciton>
+						<router-link>
+							<div>
+								<span>
+									<span>{{}}昵称</span>
+									<span>{{}}小时前</span>
+								</span>
+								<img src="static/img/.svg" alt="">
+							</div>
+							<!-- 说的内容，也可以是回复的话题 -->
+							<p>{{}}说的内容</p>
+							<seciton>
+								<!-- 发表动态的图片，如果上面是回复的话，这儿放被回复者的内容 -->
+								<img src="static/img/.svg" alt="">
+							</seciton>
+						</router-link>
+						<div>
+							<span>{{}}标签</span>
+							<span><img src="static/img/.svg" alt="">{{}}</span>
+							<span><img src="static/img/.svg" alt="">{{}}</span>
+						</div>
+					</seciton>
+				</li>
+			</ul>
+		</section>
 	</div>
 </template>
 <script>
