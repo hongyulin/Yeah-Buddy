b188bd832bf36de989716d52a1008de924cfb807
Levinson <389169974@qq.com>
2017-10-24 08:07:01 +0800

    mine

--- yeahbuddy/src/views/mine/mine.vue
+++ yeahbuddy/src/views/mine/mine.vue
@@ -1,7 +1,121 @@
 <template>
 	<!-- keep里面的我的很好， -->
 	<div>
-		我
+		<header>
+			<span>我</span>
+			<!-- 信息头部加上红色的数字 -->
+			<img src="static/img/.svg" alt="信息">
+			<img src="static/img/.svg" alt="设置">
+		</header>
+		<section>
+			<img src="static/img/.svg" alt="头像">
+			<section>
+				<span>
+					{{}}
+					<img src="static/img/.svg" alt="二维码">
+				</span>
+				<span>
+					{{}}粉丝 · {{}}关注 · {{}}动态
+				</span>
+			</section>
+			<img src="static/img/right.svg" alt="right">
+		</section>
+		<section>
+			<section>
+				<span>总运动{{}}分钟</span>
+				<span>{{}}加入keep</span>
+			</section>
+			<section>
+				<gifure>
+					<img src="static/img/.svg" alt="总等级">
+					<figcaption>总等级</figcaption>
+				</gifure>
+				<gifure>
+					<img src="static/img/.svg" alt="健身">
+					<figcaption>健身</figcaption>
+				</gifure>
+				<gifure>
+					<img src="static/img/.svg" alt="跑步">
+					<figcaption>跑步</figcaption>
+				</gifure>
+				<gifure>
+					<img src="static/img/.svg" alt="骑行">
+					<figcaption>骑行</figcaption>
+				</gifure>
+			</section>
+			<section>
+				<span>我的健康数据</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<gifure>
+					<img src="static/img/.svg" alt="运动历史">
+					<figcaption>运动历史</figcaption>
+				</gifure>
+				<gifure>
+					<img src="static/img/.svg" alt="身体数据">
+					<figcaption>身体数据</figcaption>
+				</gifure>
+				<gifure>
+					<img src="static/img/.svg" alt="运动能力">
+					<figcaption>运动能力</figcaption>
+				</gifure>
+			</section>
+			<section>
+				<span>徽章</span>
+				<span>{{}}</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+		</section>
+		<section>
+			<section>
+				<span>运动日记</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>运动概况</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>每周目标</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>链接应用与设备</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+		</section>
+		<section>
+			<section>
+				<span>我的活动</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>我的收藏</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>我跑过的路线</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+		</section>
+		<section>
+			<section>
+				<span>购物车</span>
+				<span>{{}}</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>购买记录</span>
+				<span>{{}}</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+			<section>
+				<span>优惠券</span>
+				<span>{{}}</span>
+				<img src="static/img/right.svg" alt="right">
+			</section>
+		</section>
 	</div>
 </template>
 <script>
